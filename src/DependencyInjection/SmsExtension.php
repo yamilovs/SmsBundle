d9047c10ac39cdbf3961f2d3c6da8bf51863cbe2
Yamilov Stepan <yamilovs@gmail.com>
2017-12-27 16:30:18 +0500

    Define new `provides` parameter

--- src/DependencyInjection/SmsExtension.php
+++ src/DependencyInjection/SmsExtension.php
@@ -16,7 +16,11 @@ class SmsExtension extends Extension
     {
         $config = $this->processConfiguration(new Configuration(), $configs);
 
+        // load bundle's services
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+
+        // setting up configuration
+        $container->setParameter($this->getAlias().'.providers', $config['providers']);
     }
 }
