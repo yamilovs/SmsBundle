12028d0be156107aebada2ab2a37ccc7e11aec8a
Yamilov Stepan <yamilovs@gmail.com>
2017-12-27 18:38:57 +0500

    Added a compiler pass

---
+++ src/DependencyInjection/Compiler/ProviderCompilerPass.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Yamilovs\Bundle\SmsBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Yamilovs\Bundle\SmsBundle\DependencyInjection\Factory\Provider\ProviderFactoryInterface;
+use Yamilovs\Bundle\SmsBundle\Service\ProviderManager;
+
+class ProviderCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (!$container->has(ProviderManager::class)) {
+            return;
+        }
+
+        $manager = $container->findDefinition(ProviderManager::class);
+        $providers = $container->findTaggedServiceIds(ProviderFactoryInterface::SERVICE_TAG);
+
+        foreach ($providers as $id => $tags) {
+            foreach ($tags as $attribute) {
+                $manager->addMethodCall('addProvider', [
+                    $attribute['provider'],
+                    new Reference($id),
+                ]);
+            }
+        }
+    }
+}
--- src/SmsBundle.php
+++ src/SmsBundle.php
@@ -2,8 +2,16 @@
 
 namespace Yamilovs\Bundle\SmsBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Yamilovs\Bundle\SmsBundle\DependencyInjection\Compiler\ProviderCompilerPass;
 
 class SmsBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ProviderCompilerPass());
+    }
 }
