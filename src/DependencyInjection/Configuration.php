4d0e006ca3760e92b1f066f36043ba66e5a3cd81
Yamilov Stepan <yamilovs@gmail.com>
2017-12-27 16:31:18 +0500

    Extends bundle configuration with provider factory data

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,8 +2,11 @@
 
 namespace Yamilovs\Bundle\SmsBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Yamilovs\Bundle\SmsBundle\DependencyInjection\Factory\Provider\ProviderFactoryInterface;
+use Yamilovs\Bundle\SmsBundle\DependencyInjection\Factory\Provider\SmsruProviderFactory;
 
 /**
  * This is the class that validates and merges configuration from your app/config files.
@@ -20,6 +23,37 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('yamilovs_sms');
 
+        $this->addProvidersSection($rootNode);
+
         return $treeBuilder;
+    }
+
+    private function addProvidersSection(ArrayNodeDefinition $nodeDefinition)
+    {
+        $nodeDefinition
+            ->children()
+                ->fixXmlConfig('adapter', 'adapters')
+                ->children()
+                    ->arrayNode('adapters')
+                        ->useAttributeAsKey('name')
+                        ->arrayPrototype()
+        ;
+
+        /** @var ProviderFactoryInterface $providerFactory */
+        foreach ($this->getProviderFactories() as $providerFactory) {
+            $providerFactory->addConfiguration(
+                $nodeDefinition->children()->arrayNode($providerFactory->getName())
+            );
+        }
+    }
+
+    /**
+     * @return ProviderFactoryInterface[]
+     */
+    private function getProviderFactories(): array
+    {
+        return [
+            new SmsruProviderFactory(),
+        ];
     }
 }
