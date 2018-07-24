a04c4faac4d808b9c1d421eaf746e09c2a4ec813
Yamilov Stepan <yamilovs@gmail.com>
2018-07-24 23:56:53 +0500

    Move SmsTest to a separate directory

--- src/Tests/Sms/SmsTest.php
+++ src/Tests/Sms/SmsTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Yamilovs\Bundle\SmsBundle\Tests;
+namespace Yamilovs\Bundle\SmsBundle\Tests\Sms;
 
 use PHPUnit\Framework\TestCase;
 use Yamilovs\Bundle\SmsBundle\Sms\Sms;
