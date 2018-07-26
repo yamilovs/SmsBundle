cd3bf69a1a9696b5582b64ee726a9cc8477c528c
Yamilov Stepan <yamilovs@gmail.com>
2018-07-26 14:04:54 +0500

    Using interface method in bundle configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -5,7 +5,7 @@ namespace Yamilovs\Bundle\SmsBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
-use Yamilovs\Bundle\SmsBundle\DependencyInjection\Factory\Provider\AbstractProviderFactory;
+use Yamilovs\Bundle\SmsBundle\DependencyInjection\Factory\Provider\ProviderFactoryInterface;
 
 /**
  * This is the class that validates and merges configuration from your app/config files.
@@ -15,7 +15,7 @@ use Yamilovs\Bundle\SmsBundle\DependencyInjection\Factory\Provider\AbstractProvi
 class Configuration implements ConfigurationInterface
 {
     /**
-     * @var AbstractProviderFactory[]
+     * @var ProviderFactoryInterface[]
      */
     private $providerFactoryMap = [];
 
@@ -32,12 +32,12 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $rootNode = $treeBuilder->root('yamilovs_sms');
 
-        $this->addProvidersSection($rootNode);
+        $this->buildProviderConfiguration($rootNode);
 
         return $treeBuilder;
     }
 
-    private function addProvidersSection(ArrayNodeDefinition $nodeDefinition)
+    private function buildProviderConfiguration(ArrayNodeDefinition $nodeDefinition)
     {
         $nd = $nodeDefinition
             ->fixXmlConfig('provider', 'providers')
@@ -48,7 +48,7 @@ class Configuration implements ConfigurationInterface
         ;
 
         foreach ($this->providerFactoryMap as $providerName => $providerFactory) {
-            $providerFactory->addConfiguration(
+            $providerFactory->buildConfiguration(
                 $nd->children()->arrayNode($providerName)
             );
         }
