8d7ce6d0f86b99b2b2748e9597a506e5a9795b22
Yamilov Stepan <yamilovs@gmail.com>
2018-07-30 12:03:40 +0500

    Refactor SmsCenterProvider and added more tests

--- src/Provider/SmsCenterProvider.php
+++ src/Provider/SmsCenterProvider.php
@@ -3,25 +3,13 @@
 namespace Yamilovs\Bundle\SmsBundle\Provider;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
 use Yamilovs\Bundle\SmsBundle\Exception\SmsCenterException;
 use Yamilovs\Bundle\SmsBundle\Sms\SmsInterface;
 
 class SmsCenterProvider implements ProviderInterface
 {
-    const BASE_URI = 'https://smsc.ru';
-    const SMS_SEND_URI = '/sys/send.php';
-
-    const RESPONSE_FORMAT = [
-        'STRING' => 0, // "OK - 1 SMS, ID - 12345", where 1 - message count and 12345 message id
-        'INTEGER' => 1, // "12345,1"
-        'XML' => 2, // response in XML format
-        'JSON' => 3, // response in json format
-    ];
-    const MESSAGE_CHARSET = [
-        'WINDOWS-1251' => 'windows-1251',
-        'UTF-8' => 'utf-8',
-        'KOI8-R' => 'koi8-r',
-    ];
+    private const SMS_SEND_URI = 'https://smsc.ru/sys/send.php';
 
     /**
      * @var string
@@ -44,10 +32,15 @@ class SmsCenterProvider implements ProviderInterface
     private $flash;
 
     /**
-     * @param string $login
-     *
-     * @return SmsCenterProvider
+     * @var ClientInterface
      */
+    private $client;
+
+    public function __construct()
+    {
+        $this->setClient(new Client());
+    }
+
     public function setLogin(string $login): self
     {
         $this->login = $login;
@@ -55,11 +48,6 @@ class SmsCenterProvider implements ProviderInterface
         return $this;
     }
 
-    /**
-     * @param string $password
-     *
-     * @return SmsCenterProvider
-     */
     public function setPassword(string $password): self
     {
         $this->password = $password;
@@ -67,11 +55,6 @@ class SmsCenterProvider implements ProviderInterface
         return $this;
     }
 
-    /**
-     * @param null|string $sender
-     *
-     * @return SmsCenterProvider
-     */
     public function setSender(?string $sender): self
     {
         $this->sender = $sender;
@@ -79,11 +62,6 @@ class SmsCenterProvider implements ProviderInterface
         return $this;
     }
 
-    /**
-     * @param bool $flash
-     *
-     * @return SmsCenterProvider
-     */
     public function setFlash(bool $flash): self
     {
         $this->flash = $flash;
@@ -91,30 +69,49 @@ class SmsCenterProvider implements ProviderInterface
         return $this;
     }
 
-    public function send(SmsInterface $sms)
+    public function setClient(ClientInterface $client): self
     {
-        $data = [
+        $this->client = $client;
+
+        return $this;
+    }
+
+    private function getTime(\DateTime $dateTime): string
+    {
+        // Zero at returned string mean that we send timestamp time format
+        return sprintf('0%s', $dateTime->getTimestamp());
+    }
+
+    private function getPassword(): string
+    {
+        return mb_strtolower(md5($this->password));
+    }
+
+    private function getPostData(SmsInterface $sms): array
+    {
+        $post = [
             'form_params' => [
                 'login' => $this->login,
-                'psw' => mb_strtolower(md5($this->password)),
+                'psw' => $this->getPassword(),
                 'phones' => $sms->getPhoneNumber(),
                 'mes' => $sms->getMessage(),
-                'time' => '0'.$sms->getDateTime()->getTimestamp(), // Use timestamp time format
-                'fmt' => self::RESPONSE_FORMAT['JSON'], // Get response in json format
-                'charset' => self::MESSAGE_CHARSET['UTF-8'], // Use unicode charset in message text
+                'time' => $this->getTime($sms->getDateTime()),
+                'flash' => (int)$this->flash,
+                'fmt' => 3, // Get response in json format
+                'charset' => 'utf-8', // Use unicode charset in message text
             ]
         ];
 
         if ($this->sender) {
-            $data['form_params']['sender'] = $this->sender;
+            $post['form_params']['sender'] = $this->sender;
         }
 
-        if ($this->flash) {
-            $data['form_params']['flash'] = 1;
-        }
+        return $post;
+    }
 
-        $client = new Client(['base_uri' => self::BASE_URI, 'timeout' => 10,]);
-        $response = $client->post(self::SMS_SEND_URI, $data);
+    public function send(SmsInterface $sms): bool
+    {
+        $response = $this->client->request('POST', self::SMS_SEND_URI, $this->getPostData($sms));
         $jsonResponse = json_decode($response->getBody()->getContents());
 
         if (property_exists($jsonResponse, 'error_code')) {
--- tests/Provider/SmsCenterProviderTest.php
+++ tests/Provider/SmsCenterProviderTest.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Yamilovs\Bundle\SmsBundle\Tests\Provider;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
+use Yamilovs\Bundle\SmsBundle\Exception\SmsCenterException;
 use Yamilovs\Bundle\SmsBundle\Provider\SmsCenterProvider;
+use Yamilovs\Bundle\SmsBundle\Sms\Sms;
 
 class SmsCenterProviderTest extends TestCase
 {
+    use GuzzleClientTrait;
+
     public function testThatSettersImplementsChainPattern(): void
     {
         $provider = (new SmsCenterProvider())
@@ -16,8 +22,40 @@ class SmsCenterProviderTest extends TestCase
             ->setPassword('password')
             ->setSender('sender')
             ->setFlash(true)
+            ->setClient(new Client())
         ;
 
         $this->assertInstanceOf(SmsCenterProvider::class, $provider);
+    }
+
+    public function testThatExceptionThrownOnInvalidResponseCode(): void
+    {
+        $this->expectException(SmsCenterException::class);
+
+        (new SmsCenterProvider())
+            ->setClient($this->getClientWithPreparedResponse(new Response(200, [], '{"error": "Nothing to do here", "error_code": 6}')))
+            ->send(new Sms('+1234567890', 'Hello World'))
+        ;
+    }
+
+    public function testSend(): void
+    {
+        $response = (new SmsCenterProvider())
+            ->setClient($this->getClientWithPreparedResponse(new Response(200, [], '{"id": 1, "cnt": 1}')))
+            ->send(new Sms('+1234567890', 'Hello World'))
+        ;
+
+        $this->assertTrue($response);
+    }
+
+    public function testSendWithAdditionalPostData(): void
+    {
+        $response = (new SmsCenterProvider())
+            ->setSender('sender')
+            ->setClient($this->getClientWithPreparedResponse(new Response(200, [], '{"id": 1, "cnt": 1}')))
+            ->send(new Sms('+1234567890', 'Hello World'))
+        ;
+
+        $this->assertTrue($response);
     }
 }
